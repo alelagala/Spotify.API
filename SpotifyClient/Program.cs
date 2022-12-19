48392a01f134e43e7989a7e4db4d16bf0ead915a
Alessandro La Gala <alessandro.lagala97@gmail.com>
2022-12-19 22:43:31 +0100

    Add files via upload
    "added client"

---
+++ SpotifyClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components.Routing;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Utility.Read;
+
+
+
+namespace SpotifyClient
+{
+    class Program
+    {
+        //static HttpClient client = new HttpClient();
+
+
+
+
+        static void Main()
+        {
+            UserInterface.SourceSelectionInterface();
+            var data = DataStore.GetInstance();
+            Console.WriteLine();
+            UserInterface.Router("h");
+        }
+
+
+
+    }
+}
